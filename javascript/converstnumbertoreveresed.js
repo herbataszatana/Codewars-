446e6843ba79ef9410c68b188a0f4feee691264e
Anna <anma.swiatkowska@gmail.com>
2022-08-16 23:30:46 +0100

    some more 7 kata

---
+++ javascript/converstnumbertoreveresed.js
@@ -0,0 +1,9 @@
+function digitize(n) {
+    num = n.toString();
+    arr = num.split("");
+    reversed = arr.reverse();
+    return arr.reverse();
+
+}
+
+digitize(5532);
---
+++ javascript/isthestringtouppercase.js
@@ -0,0 +1,4 @@
+String.prototype.isUpperCase = function() {
+    // your code here
+     return this.valueOf().toUpperCase() === this.valueOf();
+  }
